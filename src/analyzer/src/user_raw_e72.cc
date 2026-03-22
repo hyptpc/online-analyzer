abc8e06c0ef756127c5e6ebc5814b3d8941fc2b7
oper <oper@kazan.monitor.k18br>
2026-03-22 17:05:10 +0900

    minor change.

--- src/analyzer/src/user_raw_e72.cc
+++ src/analyzer/src/user_raw_e72.cc
@@ -1126,7 +1126,7 @@ process_event( void )
 
   gSystem->ProcessEvents();
 
-  std::cout << "\a" << std::endl;
+  std::cout << "Run#" << run_number << " Event#" << event_number << "\a" << std::endl;
 
   return 0;
 }
