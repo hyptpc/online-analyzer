d06d562952c3223dca0f6a9a1fc9eea7b77f8149
oper <oper@kazan.monitor.k18br>
2026-04-04 03:32:00 +0900

    debug port

--- src/analyzer/src/user_raw_e72_wtpc.cc
+++ src/analyzer/src/user_raw_e72_wtpc.cc
@@ -140,7 +140,7 @@ process_begin( const std::vector<std::string>& argv )
   gStyle->SetPalette(55);
   
   // unpacker and all the parameter managers are initialized at this stage  
-  int port=8087;
+  int port=9090;
   
   if(argv.size()==4){
     outputname=argv.at(3);
